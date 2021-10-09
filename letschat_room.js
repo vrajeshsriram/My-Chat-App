5ec3237139d185073e510fbffff930e5e96ed382
vrajeshsriram <71558070+vrajeshsriram@users.noreply.github.com>
2021-10-09 11:01:34 +0530

    Add files via upload

---
+++ letschat_room.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCF0xnsertR0KJ3cHi3Rgwo-2xEq7W5DGE",
+    authDomain: "kwitter-a5edf.firebaseapp.com",
+    databaseURL: "https://kwitter-a5edf-default-rtdb.firebaseio.com",
+    projectId: "kwitter-a5edf",
+    storageBucket: "kwitter-a5edf.appspot.com",
+    messagingSenderId: "400208787046",
+    appId: "1:400208787046:web:db5bd6a686d4bf4cb35fa7"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+//ADD YOUR FIREBASE LINKS HERE
